43a1b472518664d092e67af7bd4236d87045c1c9
Haoxi Tan <haoxi.tan@gmail.com>
2023-06-18 21:56:48 +1000

    runs guarddog per update

--- main.go
+++ main.go
@@ -6,9 +6,39 @@ import (
 	"io/ioutil"
 	"log"
 	"time"
+	"os/exec"
+	"strings"
 )
 
+// https://stackoverflow.com/a/41516687
+// RunCMD is a simple wrapper around terminal commands
+func RunCMD(path string, args []string, debug bool) (out string, err error) {
 
+	start := time.Now()
+    cmd := exec.Command(path, args...)
+
+    var b []byte
+    b, err = cmd.CombinedOutput()
+    out = string(b)
+
+    if debug {
+
+        log.Println(strings.Join(cmd.Args[:], " "), "command finished in:",time.Now().Sub(start))
+        if err != nil {
+            log.Println("RunCMD ERROR")
+        }
+        log.Println(out)
+    }
+
+    return
+}
+
+func process_pkg(name string, version string) {
+	log.Println("process_pkg",name,version)
+	bin := "guarddog"
+	args := []string{"npm", "scan", name, "--version", version}
+	RunCMD(bin, args, true)
+}
 
 func main() {
 	// follow changes in longpoll mode, include docs so full details are retrieved
@@ -59,6 +89,7 @@ func main() {
     			dist_tags := doc["dist-tags"].(map[string]interface{})
     			latest_ver := dist_tags["latest"]
     			log.Println(doc["name"], latest_ver)
+    			go process_pkg(doc["name"].(string), latest_ver.(string))
     		}
     		
     		// log.Println()
